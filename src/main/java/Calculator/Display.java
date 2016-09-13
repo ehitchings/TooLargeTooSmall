d42a45a9b6e62449c5bd505b7e47a67348978e9e
Evan Hitchings <evanhitchings@Evan-ZCW-11.local>
2016-09-13 09:53:49 -0400

    Please, make it stop...

--- src/main/java/Calculator/Display.java
+++ src/main/java/Calculator/Display.java
@@ -9,8 +9,8 @@ public class Display {
 
     private String currentDisplayString;
     private double currentDisplayDouble;
-    private double secondValue;
-    private String operation;
+    private double memoryValue;
+
     private BasicMath basicMath;
     private ScientificMath scientificMath;
     private Scanner reader = new Scanner(System.in);
@@ -22,18 +22,18 @@ public class Display {
     public Display(){
         this.currentDisplayString = "0";
         this.currentDisplayDouble = 0;
-        this.secondValue = 0;
-        this.operation = "";
         this.basicMath = new BasicMath();
         this.scientificMath = new ScientificMath();
         this.displayMode = "DECIMAL";
+        this.memoryValue = 0.00;
 
     }
 
     public void setCurrentDisplayString(Double currentValue) {
         this.currentDisplayString = "" + currentValue;
+        this.currentDisplayString = this.displayModeConverter();
         //this.currentDisplayString = currentDisplayString.substring(0, 9);
-        System.out.println(this);
+        System.out.println(this.currentDisplayString);
     }
 
     public String getCurrentDisplayString() {
@@ -52,6 +52,10 @@ public class Display {
      return displayModeConverter();
 
 
+    }
+
+    public void clearMemoryValue() {
+        this.memoryValue = 0.00;
     }
 
     public String displayModeConverter(){
@@ -77,18 +81,21 @@ public class Display {
         for (int i = 0; i < 20; i++){
             System.out.println();
         }
-        this.setCurrentDisplayString(0.0);
-        this.currentDisplayDouble = 0;
-        this.secondValue = 0;
+        setCurrentDisplayDouble(0.00);
+        //this.setCurrentDisplayString(0.0);
+
+
     }
 
 
+    public void setMemoryValue(double memoryValue) {
+        this.memoryValue = memoryValue;
+    }
 
-
-    public void setCurrentDisplayDouble(double currentDisplayDouble) {
-        this.currentDisplayDouble = currentDisplayDouble;
+    public void setCurrentDisplayDouble(double displayDouble) {
+        this.currentDisplayDouble = displayDouble;
         setCurrentDisplayString(currentDisplayDouble);
-        System.out.println(currentDisplayString);
+        //System.out.println(currentDisplayString);
 
     }
 
@@ -100,7 +107,7 @@ public class Display {
         String command;
         do {
             System.out.println(currentDisplayString);
-            System.out.println("Choose an option: \n 1: Basic Math \n 2: Scientific Math \n 3: Change Display Mode \n C: Clear Screen \n Q: Quit");
+            System.out.println("Choose an option: \n 1: Basic Math \n 2: Scientific Math \n C: Clear Screen \n Q: Quit");
             command = reader.nextLine();
 
             switch (command){
@@ -110,12 +117,11 @@ public class Display {
                 case "2":
                     scientificMathMenu();
                     break;
-                case "3":
-                    //changeDisplayMenu();
-                    break;
+                case "c":
                 case "C":
                     clearDisplay();
                     break;
+                case "q":
                 case "Q":
                     return;
                 default:
@@ -133,60 +139,79 @@ public class Display {
     public void basicMathMenu(){
 
         String command;
+        setCurrentDisplayString(currentDisplayDouble);
        do {
            if (currentDisplayDouble == 0){
                setCurrentDisplayDouble(numberInput());
                setCurrentDisplayString(currentDisplayDouble);
+           } else {
+               System.out.println(currentDisplayString);
+
            }
-           System.out.println(currentDisplayString);
            System.out.println("Choose an option: \n 1: Add \n 2: Subtract \n 3: Multiply \n 4: Divide \n 5: Square \n 6: SquareRoot \n 7: Exponent \n " +
-                   "8: Inverse \n S: Switch Display Mode \n C: Clear \n Q: Previous");
+                   "8: Inverse \n M+: Add Mem \n MC: Memory Clear \n MR: Memory Recall \n S: Switch Display Mode (Cycle) \n M: Switch Display Mode (Manual) \n C: Clear \n Q: Previous");
            command = reader.nextLine();
            switch (command){
                case "1":
-                   currentDisplayDouble = basicMath.add(numberInput(), currentDisplayDouble);
+                   setCurrentDisplayDouble(basicMath.add(numberInput(), currentDisplayDouble));
                    break;
                case "2":
-                   currentDisplayDouble = basicMath.subtract(numberInput(), currentDisplayDouble);
+                   setCurrentDisplayDouble(basicMath.subtract(currentDisplayDouble, numberInput()));
                    break;
                case "3":
-                   currentDisplayDouble = basicMath.multiply(numberInput(), currentDisplayDouble);
+                   setCurrentDisplayDouble(basicMath.multiply(numberInput(), currentDisplayDouble));
                    break;
                case "4":
                    try{
-                       currentDisplayDouble = basicMath.divide(numberInput(), currentDisplayDouble);
+                       setCurrentDisplayDouble(basicMath.divide(currentDisplayDouble, numberInput()));
                    } catch (ArithmeticException e){
                        currentDisplayString = "err";
                        break;
                    }
                    break;
                case "5":
-                   currentDisplayDouble = basicMath.square(currentDisplayDouble);
+                   setCurrentDisplayDouble(basicMath.square(currentDisplayDouble));
                    break;
                case "6":
-                   currentDisplayDouble = basicMath.squareRoot(currentDisplayDouble);
+                   setCurrentDisplayDouble(basicMath.squareRoot(currentDisplayDouble));
                    break;
                case "7":
-                   currentDisplayDouble = basicMath.exponation(currentDisplayDouble, numberInput());
+                   setCurrentDisplayDouble(basicMath.exponation(currentDisplayDouble, numberInput()));
                    break;
                case "8":
-                   currentDisplayDouble = basicMath.inverse(currentDisplayDouble);
+                   setCurrentDisplayDouble(basicMath.inverse(currentDisplayDouble));
                    break;
+               case "m+":
+               case "M+":
+                   setMemoryValue(basicMath.add(currentDisplayDouble, memoryValue));
+                   break;
+               case "mc":
+               case "MC":
+                   clearMemoryValue();
+                   break;
+               case "mr":
+               case "MR":
+                   setCurrentDisplayDouble(memoryValue);
+                   break;
+               case "s":
                case "S":
-                   //switchDisplayLogic();
+                   switchDisplayMode();
                    break;
+               case "c":
                case "C":
                    clearDisplay();
                    break;
+               case "q":
                case "Q":
                    return;
                default:
                    break;
            }
            if(!(currentDisplayString.equalsIgnoreCase("err"))){
-               //setCurrentDisplayString(currentDisplayDouble);
+               setCurrentDisplayString(currentDisplayDouble);
                continue;
            }
+           //setCurrentDisplayString(currentDisplayDouble);
 
 
        } while (!(command.equalsIgnoreCase("Q")));
@@ -200,67 +225,93 @@ public class Display {
             if (currentDisplayDouble == 0) {
                 setCurrentDisplayDouble(numberInput());
                 setCurrentDisplayString(currentDisplayDouble);
+            } else {
+                System.out.println(currentDisplayString);
             }
-            System.out.println(currentDisplayString);
+
             System.out.println("Choose an option: \n 1: Sin \n 2: Cos \n 3: Tan \n 4: Inverse Sin \n 5: Inverse Cos \n 6: Inverse Tan \n T: Change Trig Mode \n " +
-                    "8: Log \n 9: Natural Log \n 10: Inverse Log \n 11: Inverse Natural Log \n 12: Facotiral \n S: Switch Display Mode \n M: Manual Switch Display Mode\n C: Clear \n Q: Previous");
+                    "8: Log \n 9: Natural Log \n 10: Inverse Log \n 11: Inverse Natural Log \n 12: Facotiral \n " +
+                    "M+: Add Mem \n MC: Memory Clear \n MR: Memory Recall \n S: Switch Display Mode \n M: Manual Switch Display Mode\n C: Clear \n Q: Previous");
             command = reader.nextLine();
             try {
 
                 switch (command) {
                     case "1":
-                        currentDisplayDouble = scientificMath.sin(currentDisplayDouble);
+                        setCurrentDisplayDouble(scientificMath.sin(currentDisplayDouble));
                         break;
                     case "2":
-                        currentDisplayDouble = scientificMath.cos(currentDisplayDouble);
+                        setCurrentDisplayDouble(scientificMath.cos(currentDisplayDouble));
                         break;
                     case "3":
-                        currentDisplayDouble = scientificMath.tan(currentDisplayDouble);
+                        setCurrentDisplayDouble(scientificMath.tan(currentDisplayDouble));
                         break;
                     case "4":
-                        currentDisplayDouble = scientificMath.asin(currentDisplayDouble);
+                        setCurrentDisplayDouble(scientificMath.asin(currentDisplayDouble));
                         break;
                     case "5":
-                        currentDisplayDouble = scientificMath.acos(currentDisplayDouble);
+                        setCurrentDisplayDouble(scientificMath.acos(currentDisplayDouble));
                         break;
                     case "6":
-                        currentDisplayDouble = scientificMath.atan(currentDisplayDouble);
+                        setCurrentDisplayDouble(scientificMath.atan(currentDisplayDouble));
                         break;
-                    case "7":
+                    case "t":
+                    case "T":
                         clearDisplay();
                         System.out.println(this.currentDisplayString);
-                        System.out.println("1: RADIANS \n 2: DEGREES");
-                        String modeSwitchCommand = "";
-                        while(!(modeSwitchCommand.equalsIgnoreCase("1")) || !(modeSwitchCommand.equalsIgnoreCase("2")));
-                        modeSwitchCommand = reader.nextLine();
+                        System.out.println("1: RADIANS \n2: DEGREES");
+                        String modeSwitchCommand = reader.nextLine();
+                        switch(modeSwitchCommand){
+                            case "1":
+                                scientificMath.switchUnitsMode("1");
+                                break;
+                            case "2":
+                                scientificMath.switchUnitsMode("2");
+                                break;
+                            default:
+                                break;
+                        }
 
-                        scientificMath.switchUnitsMode(modeSwitchCommand);
-                        break;
                     case "8":
-                        currentDisplayDouble = scientificMath.log(currentDisplayDouble);
+                        setCurrentDisplayDouble(scientificMath.log(currentDisplayDouble));
                         break;
                     case "9":
-                        currentDisplayDouble = scientificMath.ln(currentDisplayDouble);
+                        setCurrentDisplayDouble(scientificMath.ln(currentDisplayDouble));
                         break;
                     case "10":
-                        currentDisplayDouble = scientificMath.inverseLog(currentDisplayDouble);
+                        setCurrentDisplayDouble(scientificMath.inverseLog(currentDisplayDouble));
                         break;
                     case "11":
-                        currentDisplayDouble = scientificMath.inverseLn(currentDisplayDouble);
+                        setCurrentDisplayDouble(scientificMath.inverseLn(currentDisplayDouble));
                         break;
                     case "12":
-                        currentDisplayDouble = scientificMath.factorial(currentDisplayDouble);
+                        setCurrentDisplayDouble(scientificMath.factorial(currentDisplayDouble));
                         break;
+                    case "m+":
+                    case "M+":
+                        setMemoryValue(basicMath.add(currentDisplayDouble, memoryValue));
+                        break;
+                    case "mc":
+                    case "MC":
+                        clearMemoryValue();
+                        break;
+                    case "mr":
+                    case "MR":
+                        setCurrentDisplayDouble(memoryValue);
+                        break;
+                    case "s":
                     case "S":
                         switchDisplayMode();
                         break;
+                    case "m":
                     case "M":
                         String displayCommand = reader.nextLine();
                         switchDisplayMode(displayCommand);
-
+                        break;
+                    case "c":
                     case "C":
                         clearDisplay();
                         break;
+                    case "q":
                     case "Q":
                         return;
                     default:
